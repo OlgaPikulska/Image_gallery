b62a1ff9b017e713ee24dd438530b629d493277c
Olga Pikulska <olga.pikulska.ld@gmail.com>
2022-11-04 15:10:09 +0100

    task 1 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,36 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map(
+    (item) =>
+      `<div class="gallery__item"><a class="gallery__link" href=${item.original}><img class="gallery__image" src=${item.preview} data-source=${item.original} alt=${item.description}/></a></div>`
+  )
+  .join("");
+gallery.innerHTML = markup;
+
+let instance;
+
+gallery.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const modal = document.createElement("div");
+  const newPic = document.createElement("img");
+  newPic.src = event.target.getAttribute("data-source");
+  modal.append(newPic);
+
+  instance = basicLightbox.create(modal);
+  instance.show();
+});
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Escape") {
+    instance.close();
+  }
+});
